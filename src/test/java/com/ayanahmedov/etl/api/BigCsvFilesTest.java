dbcc4003366c59fa592c591e3144ab6513f08cc9
Ayan Ahmedov <ayan.ahmedov@external.t-mobile.at>
2019-07-31 21:09:47 +0200

    demo-etl | add bulk concurrent tests

--- src/test/java/com/ayanahmedov/etl/api/BigCsvFilesTest.java
+++ src/test/java/com/ayanahmedov/etl/api/BigCsvFilesTest.java
@@ -10,18 +10,23 @@ import org.junit.jupiter.api.Timeout;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class BigCsvFilesTest {
   private static int NUM_CSV_ROWS = 100_00;
 
   private static Path bigCsvPath;
-  private static Path configFile = TestUtils.getFileFromResources("test-dsl-instance-1.xml");
 
   @BeforeAll
   public static void createHugeCsv() {
@@ -53,6 +58,8 @@ public class BigCsvFilesTest {
   @DisplayName("transform big sourceCsv to targetCsv")
   @RepeatedTest(50)
   public void transform_Big_Csv_to_targetCsvFile() throws IOException {
+    Path configFile = TestUtils.getFileFromResources("test-dsl-instance-1.xml");
+
     CsvTransformer transformer = new CsvTransformerBuilder()
         .withXmlDsl(configFile)
         .withSourceCsvValueMappers(Collections.singletonList(new TwoDigitsNormalizer()))
@@ -81,5 +88,103 @@ public class BigCsvFilesTest {
     TestUtils.assertStringEqualsCsv(lastLine, Collections.singletonList(
         new String[]{"1000","2018-01-01","P-1000","Arugola","5250.5","kg"}
     ));
+  }
+
+  @Timeout(value = 2, unit = TimeUnit.MINUTES)
+  @DisplayName("transform big sourceCsv to targetCsv")
+  @RepeatedTest(50)
+  public void concurrent_transformations_with_different_dsls() throws IOException {
+    Path configFile1 = TestUtils.getFileFromResources("test-dsl-instance-concurrent-1.xml");
+    Path configFile2 = TestUtils.getFileFromResources("test-dsl-instance-concurrent-2.xml");
+    Path configFile3 = TestUtils.getFileFromResources("test-dsl-instance-concurrent-3.xml");
+
+    CsvTransformer transformer1 = new CsvTransformerBuilder()
+        .withXmlDsl(configFile1)
+        .withObjectConstructors(Collections.emptyList())
+        .build();
+
+    CsvTransformer transformer2 = new CsvTransformerBuilder()
+        .withXmlDsl(configFile2)
+        .withObjectConstructors(Collections.emptyList())
+        .build();
+
+    CsvTransformer transformer3 = new CsvTransformerBuilder()
+        .withXmlDsl(configFile3)
+        .withObjectConstructors(Collections.emptyList())
+        .build();
+
+
+    Path outputTemp1 = TestUtils.createTemporaryCsvFile(printer -> {
+//      do nothing
+    });
+
+    Path outputTemp2 = TestUtils.createTemporaryCsvFile(printer -> {
+//      do nothing
+    });
+
+    Path outputTemp3 = TestUtils.createTemporaryCsvFile(printer -> {
+//      do nothing
+    });
+
+    try (
+        BufferedWriter writer1 = Files.newBufferedWriter(outputTemp1);
+        BufferedWriter writer2 = Files.newBufferedWriter(outputTemp2);
+        BufferedWriter writer3 = Files.newBufferedWriter(outputTemp3);
+        BufferedReader reader1 = Files.newBufferedReader(bigCsvPath);
+        BufferedReader reader2 = Files.newBufferedReader(bigCsvPath);
+        BufferedReader reader3 = Files.newBufferedReader(bigCsvPath)) {
+
+
+      Stream.of(
+          TransformConcurrentContainer.of(transformer1, writer1, reader1),
+          TransformConcurrentContainer.of(transformer2, writer2, reader2),
+          TransformConcurrentContainer.of(transformer3, writer3, reader3))
+          .parallel()
+          .forEach(pair -> pair.transformer.transform(pair.csvReader, pair.outputWriter));
+    }
+
+    BufferedReader outputReader1 = Files.newBufferedReader(outputTemp1);
+    BufferedReader outputReader2 = Files.newBufferedReader(outputTemp2);
+    BufferedReader outputReader3 = Files.newBufferedReader(outputTemp3);
+
+    assertAll(Arrays.asList(
+        () ->  TestUtils.assertStringEqualsCsv(outputReader1.readLine(), Collections.singletonList(
+            new String[]{"Unit","OrderDate"}
+        )),
+
+        () ->  TestUtils.assertStringEqualsCsv(outputReader2.readLine(), Collections.singletonList(
+            new String[]{"Unit","OrderDate"}
+        )),
+
+        () -> TestUtils.assertStringEqualsCsv(outputReader3.readLine(), Collections.singletonList(
+            new String[]{"Unit","OrderDate"}
+        )),
+
+        () -> TestUtils.assertStringEqualsCsv(TestUtils.readLastLineOfFile(outputTemp1), Collections.singletonList(
+            new String[]{"01-01-2018","gram"}
+        )),
+
+        () -> TestUtils.assertStringEqualsCsv(TestUtils.readLastLineOfFile(outputTemp2), Collections.singletonList(
+            new String[]{"01-01-2018","pound"}
+        )),
+
+        () -> TestUtils.assertStringEqualsCsv(TestUtils.readLastLineOfFile(outputTemp3), Collections.singletonList(
+            new String[]{"01-01-2018","ton"}
+        ))
+    ));
+  }
+
+  static class TransformConcurrentContainer {
+    CsvTransformer transformer;
+    Writer outputWriter;
+    Reader csvReader;
+
+    public static TransformConcurrentContainer of(CsvTransformer transformer, Writer outputWriter, Reader csvReader) {
+      TransformConcurrentContainer p = new TransformConcurrentContainer();
+      p.transformer = transformer;
+      p.outputWriter = outputWriter;
+      p.csvReader = csvReader;
+      return p;
+    }
   }
 }
